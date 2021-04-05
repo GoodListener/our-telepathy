2ef4806d39af64a6527a510ec52db402cd057994
ekim <good_listener@naver.com>
2021-04-05 11:42:09 +0900

    Updates

---
+++ precache-manifest.eec9e1a347b4bbb85a8d5c7d3384a296.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3018321b729291a7615fdf67cea7f78f",
+    "url": "/our-telepathy/index.html"
+  },
+  {
+    "revision": "3c3ad8c2329c2373fd90",
+    "url": "/our-telepathy/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6b7e2354b4791083946f",
+    "url": "/our-telepathy/static/js/2.65ddb39c.chunk.js"
+  },
+  {
+    "revision": "978ae41baecce37555c8d3e662bd9373",
+    "url": "/our-telepathy/static/js/2.65ddb39c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c3ad8c2329c2373fd90",
+    "url": "/our-telepathy/static/js/main.163b9277.chunk.js"
+  },
+  {
+    "revision": "54202b9159d2389c8e09",
+    "url": "/our-telepathy/static/js/runtime-main.b0182171.js"
+  }
+]);
